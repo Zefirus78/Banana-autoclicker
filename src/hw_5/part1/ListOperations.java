89e4a6e70bef49343c46abb28bd8a5c3560d6c45
Nick Shterbina <nick.shterbina04@gmail.com>
2024-05-07 18:52:30 +0300

    Fixed variable names in ListOperations

--- src/hw_5/part1/ListOperations.java
+++ src/hw_5/part1/ListOperations.java
@@ -4,36 +4,36 @@ import java.util.*;
 
 public class ListOperations {
 
-    public int countOccurrence(List<String> list, String str){
+    public int countOccurrence(List<String> listOfOccurrences, String occurrenceWord){
         int count = 0;
-        for(String s : list){
-            if(s.equals(str)){
+        for(String s : listOfOccurrences){
+            if(s.equals(occurrenceWord)){
                 count++;
             }
         }
         return count;
     }
 
-    public List<Integer> toList(int[] arr){
+    public List<Integer> toList(int[] arrayOfIntegers){
         List<Integer> list = new ArrayList<Integer>();
-        for(int i : arr){
+        for(int i : arrayOfIntegers){
             list.add(i);
         }
         return list;
     }
 
-    public Set<Integer> findUnique(List<Integer> list){
+    public Set<Integer> findUnique(List<Integer> listOfIntegers){
         Set<Integer> uniqueNumbers = new HashSet<Integer>();
-        for(int i : list){
+        for(int i : listOfIntegers){
             uniqueNumbers.add(i);
         }
         return uniqueNumbers;
     }
     // how can I insert that Map into list? Because i have no idea how to do that :)
-    public void findOccurrence(List<String> list){
+    public void findOccurrence(List<String> listOfOccurrences){
         Map<String, Integer> occurrences = new HashMap<String, Integer>();
         List<String> result = new ArrayList<>();
-        for(String s : list){
+        for(String s : listOfOccurrences){
             occurrences.put(s, occurrences.getOrDefault(s, 0) + 1);
         }
         for(Map.Entry<String, Integer> entry : occurrences.entrySet()){
